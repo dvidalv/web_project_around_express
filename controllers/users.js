64f72ab7da681b5013c6907368204ce9178974a1
dvidalv <dvidalv@gmail.com>
2023-12-30 08:52:42 -0400

    Refactor user controller functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,12 @@
 const httpStatus = require('http-status');
 const User = require('../models/user');
 
-// GET /users — devuelve a todos los usuarios
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({}).orFail(() => {
       const error = new Error('No se ha encontrado ningún usuario');
       error.statusCode = 404;
-      throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+      throw error;
     });
     return res.status(httpStatus.OK).json({
       status: 'success',
@@ -22,14 +21,13 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-// GET /users/:id — devuelve información sobre el usuario
 const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id).orFail(() => {
       const error = new Error('No se ha encontrado ningun usuario con esa id');
       error.statusCode = 404;
-      throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+      throw error;
     });
     if (!user) {
       return res.status(httpStatus.NOT_FOUND).json({
@@ -46,12 +44,11 @@ const getUserById = async (req, res) => {
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
       status: 'error',
       message: 'Unexpected error',
-      error: err.message, // Añade el mensaje de error
+      error: err.message,
     });
   }
 };
 
-// POST /users — crea un usuario
 const createUser = async (req, res) => {
   try {
     const { name, about, avatar } = req.body;
@@ -81,14 +78,13 @@ const createUser = async (req, res) => {
   }
 };
 
-// DELETE /users/:id — elimina un usuario
 const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findByIdAndDelete(id).orFail(() => {
       const error = new Error('No se ha encontrado ninguna tarjeta con esa id');
       error.statusCode = 404;
-      throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+      throw error;
     });
     if (!user) {
       return res.status(httpStatus.NOT_FOUND).json({
@@ -110,7 +106,6 @@ const deleteUser = async (req, res) => {
   }
 };
 
-// PATCH /users/me — actualiza el perfil
 const updateProfile = async (req, res) => {
   try {
     const { name, about } = req.body;
@@ -121,7 +116,7 @@ const updateProfile = async (req, res) => {
     ).orFail(() => {
       const error = new Error('No se ha encontrado ninguna tarjeta con esa id');
       error.statusCode = 404;
-      throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+      throw error;
     });
     if (!user) {
       return res.status(httpStatus.NOT_FOUND).json({
@@ -143,8 +138,6 @@ const updateProfile = async (req, res) => {
   }
 };
 
-// PATCH /users/me/avatar — actualiza el avatar
-
 const updateAvatar = async (req, res) => {
   try {
     const { avatar } = req.body;
@@ -156,7 +149,7 @@ const updateAvatar = async (req, res) => {
     ).orFail(() => {
       const error = new Error('No se ha encontrado ninguna tarjeta con esa id');
       error.statusCode = 404;
-      throw error; // Recuerda arrojar un error para que .catch lo maneje en lugar de .then
+      throw error;
     });
     if (!user) {
       return res.status(httpStatus.NOT_FOUND).json({
